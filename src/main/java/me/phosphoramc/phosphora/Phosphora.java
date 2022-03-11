1386fd634fe0100092f7c9200f2be2a32d4a72e2
Niko <bow682@gmail.com>
2022-03-10 19:04:02 -0500

    learning learning

--- src/main/java/me/phosphoramc/phosphora/Phosphora.java
+++ src/main/java/me/phosphoramc/phosphora/Phosphora.java
@@ -1,9 +1,9 @@
 package me.phosphoramc.phosphora;
 
+import me.phosphoramc.phosphora.commands.FeedCommand;
+import me.phosphoramc.phosphora.commands.GodCommand;
 import me.phosphoramc.phosphora.listeners.JoinLeaveListener;
 import me.phosphoramc.phosphora.listeners.XPBottleBreakListener;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Phosphora extends JavaPlugin {
@@ -14,12 +14,8 @@ public final class Phosphora extends JavaPlugin {
         System.out.println("Welcome to the Wilds");
         getServer().getPluginManager().registerEvents(new XPBottleBreakListener(),this);
         getServer().getPluginManager().registerEvents(new JoinLeaveListener(),this);
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-        return true;
+        getCommand("god").setExecutor(new GodCommand());
+        getCommand("eat").setExecutor(new FeedCommand());
     }
 }
 
